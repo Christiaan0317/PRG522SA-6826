67d396aa9e5043e5db9f65bf3a28d668614f7823
Christiaan0317 <66475599+Christiaan0317@users.noreply.github.com>
2021-09-15 11:03:47 +0200

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRG522SA_6826.Startup))]
+namespace PRG522SA_6826
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
